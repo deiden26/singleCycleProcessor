ce515fe6e96ef66aad559d13ef9b7d49fa637e16
Danny Eiden <deiden26@gmail.com>
2015-02-20 18:34:55 -0600

    switch to [small number:large number] in ifu

--- components/ifu.v
+++ components/ifu.v
@@ -18,21 +18,21 @@ module ifu(
     input         fp_branch,              // taken-branch signal for fpu 
     input         jump,                   // jump signal
 	input         use_reg,                // if JR or JALR
-    input  [31:0] pc_from_reg,            // use if use_reg is TRUE
-    input  [31:0] inst_from_mem,          // Data coming back from instruction-memory
+    input  [0:31] pc_from_reg,            // use if use_reg is TRUE
+    input  [0:31] inst_from_mem,          // Data coming back from instruction-memory
 
-    output [31:0] pc_to_mem,              // Address sent to Instruction memory
-    output [31:0] pc_8_out,               // PC of to store in reg31 for JAL & JALR (PC+8)
-    output [31:0] inst_out                // fetched instruction out
+    output [0:31] pc_to_mem,              // Address sent to Instruction memory
+    output [0:31] pc_8_out,               // PC of to store in reg31 for JAL & JALR (PC+8)
+    output [0:31] inst_out                // fetched instruction out
   );
 
 
-  wire    [31:0] pc_plus_4;               // Default next pc
+  wire    [0:31] pc_plus_4;               // Default next pc
 
-  reg     [31:0] next_pc;                 // Actual next pc
-  reg     [31:0] current_pc;              // PC we are currently fetching
+  reg     [0:31] next_pc;                 // Actual next pc
+  reg     [0:31] current_pc;              // PC we are currently fetching
 
-  assign pc_to_mem = {current_pc[31:2], 2'b0};
+  assign pc_to_mem = {current_pc[2:31], 2'b0};
 
   // pass instruction fetched from memory to output (assumes memory passes back 32 bits)
   assign inst_out = inst_from_mem;
@@ -45,15 +45,15 @@ module ifu(
 
   // calculate next pc
   always@(gp_branch or fp_branch or jump or use_reg or pc_plus_4 or inst_out or pc_from_reg) begin
-	  //If branching, next_pc = pc + 4 + signExtend(inst_out[15:0])
+	  //If branching, next_pc = pc + 4 + signExtend(inst_out[0:15])
 	  if (gp_branch || fp_branch)
-		  next_pc <= pc_plus_4 + inst_out[15:0];
+		  next_pc <= pc_plus_4 + inst_out[0:15];
 	  //If JALR or JR, next_pc = reg31
 	  else if (jump && use_reg)
 		  next_pc <= pc_from_reg;
-	  //If jumping (without reg), next_pc = pc + 4 + signExtend(inst_out[25:0])
+	  //If jumping (without reg), next_pc = pc + 4 + signExtend(inst_out[0:25])
 	  else if (jump)
-		  next_pc <= pc_plus_4 + inst_out[25:0];
+		  next_pc <= pc_plus_4 + inst_out[0:25];
 	  //Default: move to next word in mem, pc = pc + 4
 	  else
 		  next_pc <= pc_plus_4;
